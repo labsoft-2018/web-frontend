aa0c666a84312a7a23579afd7f763a7df1e786d3
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-23 01:35:35 -0200

    Remove dashboard plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,10 @@ const webpack = require('webpack');
 const path = require('path');
 const Merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.config')
-const DashboardPlugin = require('webpack-dashboard/plugin');
-const Dashboard = require('webpack-dashboard');
+// const DashboardPlugin = require('webpack-dashboard/plugin');
+// const Dashboard = require('webpack-dashboard');
 
-const dashboard = new Dashboard()
+// const dashboard = new Dashboard()
 
 const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, 'src');
@@ -39,10 +39,10 @@ module.exports = Merge(commonConfig, {
         NODE_ENV: JSON.stringify('development'),
       },
     }),
-    new DashboardPlugin({
-      port: 8080,
-      handler: dashboard.setData
-    })
+    // new DashboardPlugin({
+    //   port: 8080,
+    //   handler: dashboard.setData
+    // })
   ],
   // Enable sourcemaps for debugging webpack's output.
   devtool: 'source-map',  
